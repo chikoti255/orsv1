2b702cb6775e816f28c304332f39ec03f48ea7cb
kelvin magwe <kelvinmullar5@gmail.com>
2024-08-16 10:29:28 +0300

    modified background of admin page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin Login</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/admin-login.css') }}" />
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -13,74 +15,27 @@
         use Illuminate\Support\Facades\Session;
     @endphp
   <style>
-      body {
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
 
-      }
-      .loader {
-          /*width: 100vw;
-          height: 100vh;*/
-          top: 0;
-          left: 0;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background-color: #333333;
-          transition: opacity 0.75s, visibility 0.75s;
-      }
-      .loader::after {
-          content: "";
-          width: 70px;
-          height: 70px;
-          border: 10px solid #dddddd;
-          border-top-color: #3399cc;
-          border-radius: 50%;
-          animation: loading 0.75s ease infinite;
-      }
-      .loader--hidden {
-        opacity: 0;
-        visibility: hidden;
-      }
-      @keyframes loading {
-          from{ transform: rotate(0turn) }
-          to{ transform: rotate(1turn) }
-      }
-
-      #login-form {
-   animation: fadeInFromTop 1s ease-out forwards;
-   max-width: 400px;
-   width: 100%;
-
- }
-
- @keyframes fadeInFromTop {
-   0% {
-     opacity: 0;
-     transform: translateY(-90px);
-   }
-   100% {
-     opacity: 1;
-     transform: translateY(0);
-   }
- }
   </style>
 </head>
 <body>
   <x-auth-session-status class="mb-4" :status="session('status')" />
 
+  <div class="video-container">
+        <video autoplay muted loop class="background-video">
+            <source src="{{ asset('./images/admin-background.mp4') }}" />
+              Your browser does not support video tag
+        </video>
+  </div>
+
 
     <div class="loader loader--hidden"></div>
 
-      <div class="flex justify-center items-center">
-          <div id="login-form" class="bg-white rounded-lg shadow-md px-10 py-8 mx-auto w-full max-w-md">
-                 <h1 class="text-center text-2xl font-bold mb-6">Welcome Back!</h1>
-                 <h2 class="text-center text-xl mb-6">Please Sign in to continue</h2>
+      <div class="flex justify-center items-center content">
+          <div id="login-form" class="rounded-lg shadow-md px-10 py-8 mx-auto w-full max-w-md">
+                  <h1 class="text-center text-4xl font-bold mb-4">EACA 2024</h1>
+                  <h2 class="text-center text-2xl font-bold mb-2">Welcome Back!</h1>
+                  <h3 class="text-center text-xl mb-4">Please Sign in to continue</h2>
 
                  @if(Session::has('admin_login_error'))
                          <div class="alert alert-warning alert-dismissible fade show" role="alert">
