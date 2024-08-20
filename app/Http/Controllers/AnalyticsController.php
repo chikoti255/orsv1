7cd1e79a9c2da2e7625f40ed960b96789339e4ff
kelvin magwe <kelvinmullar5@gmail.com>
2024-08-20 16:45:51 +0300

    Complete optimization for Country Eager loading queries and initialized bar chart

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\RegisterAttendee;
+use Illuminate\Support\Facades\DB;
 
 class AnalyticsController extends Controller
 {
@@ -17,7 +18,28 @@ class AnalyticsController extends Controller
         $payed = RegisterAttendee::where('confirm_payment', 'YES, i Complete my payment')->count();
         $notPayed= RegisterAttendee::where('confirm_payment', 'No, I will pay on arrival')->count();
 
-        return view('analytics.analytics', compact('eacaCount','nonEacaCount','studentCount','payed','notPayed'));
+        //optimization for Eager loading (fetching all the attendees and count them by grouping countries)
+        $attendeesByCountry = RegisterAttendee::select('country', DB::raw('count(*)'))
+                              ->groupBy('country')
+                              ->get();
+
+        $china= $attendeesByCountry->where('country','China')->first()->count() ?? 0;
+        $ethiopia= $attendeesByCountry->where('country','Ethiopia')->first()->count() ?? 0;
+        $kenya= $attendeesByCountry->where('country','Kenya')->first()->count() ?? 0;
+        $nigeria= $attendeesByCountry->where('country','Nigeria')->first()->count() ?? 0;
+        $norway= $attendeesByCountry->where('country', 'Norway')->first()?->count() ?? 0;
+        $rwanda= $attendeesByCountry->where('country','Rwanda')->first()->count() ?? 0;
+        $southAfrica= $attendeesByCountry->where('country','China')->first()->count() ?? 0;
+        $sweden= $attendeesByCountry->where('country','Sweden')->first()?->count() ?? 0;
+        $tanzania= $attendeesByCountry->where('country','Tanzania')->first()->count() ?? 0;
+        $uganda= $attendeesByCountry->where('country','Uganda')->first()->count() ?? 0;
+        $uk= $attendeesByCountry->where('country','United Kingdom')->first()->count() ?? 0;
+        $netherlands= $attendeesByCountry->where('country','Netherlands')->first()?->count() ?? 0;
+
+
+        return view('analytics.analytics',
+              compact('eacaCount','nonEacaCount','studentCount','payed','notPayed',
+                        'china','ethiopia','kenya','rwanda','nigeria','norway','southAfrica','sweden','tanzania','uganda','uk','netherlands'));
 
     }
 }
--- resources/views/analytics/analytics.blade.php
+++ resources/views/analytics/analytics.blade.php
@@ -17,9 +17,9 @@
 
 </div>
 
-<div class="bar-chart">
-
-</div>
+    <div class="bar-chart">
+        <div id="columnchart_values" style="width: 900px; height: 500px;"></div>
+    </div>
 
 <script>
 
@@ -40,6 +40,22 @@ function drawChart() {
       ['No, I will pay on Arrival',      {{ $notPayed }}]
     ]);
 
+    var countrys = google.visualization.arrayToDataTable([
+        ["Country", "counts", { role: "style" } ],
+        ["China", {{ $china }}, "#b87333"],
+        ["Ehiopia", {{ $ethiopia }}, "#C0C0C"],
+        ["Kenya", {{ $kenya }}, " #FFD700"],
+        ["Rwanda", {{ $rwanda }}, "color: #e5e4e2"],
+        ["Nigeria", {{ $nigeria }}, '#008080'],
+        ["Norway", {{ $norway }}, '#332CD32'],
+        ["South Africa", {{ $southAfrica }}, '#3FF0000'],
+        ["Sweden", {{ $sweden }}, '#FFA500'],
+        ["Tanzania", {{ $tanzania }}, '#800080'],
+        ["Uganda", {{ $uganda }}, '#00FFFF'],
+        ["United Kingdoms", {{ $uk }}, '#000080'],
+        ["Netherlands", {{ $netherlands }}, '#FF69B4'],
+      ]);
+
     var pie = {
       title: 'Memberships Counts',
       is3D: true,
@@ -50,11 +66,33 @@ function drawChart() {
           pieHole: 0.4,
         };
 
+    var bar = {
+      title: "Countries Statistics of EACA Attendees",
+      width: 600,
+      height: 400,
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    }
+
     var pieChart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
     pieChart.draw(memberships, pie);
 
     var donutChart = new google.visualization.PieChart(document.getElementById('donutchart'));
         donutChart.draw(payments, donut);
+
+
+    //for bar graph
+    var barGraphView = new google.visualization.DataView(countrys);
+      barGraphView.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+            var barChart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+             barChart.draw(barGraphView, bar);
+
 }
 
 </script>
